53beb12b4d184c4802f7a0cb8d90cbc3f7cfda1b
cedzhang <cedzhang@berkeley.edu>
2018-05-01 15:03:10 -0700

    Add files via upload

---
+++ boundary_to_trace_v4.py
@@ -0,0 +1,67 @@
+import z3
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+def pre_process(boundary, eps): #boundary is a 2-d array: [[x_1, y_1],[x_2,y_2], ...]. Normally, x is tau and y is h.
+    size = len(boundary)
+    b_plus = [[0, 0] for i in range(len(boundary)-1)]
+    b_minus = [[0, 0] for i in range(len(boundary)-1)]
+    for i in range(size - 1):
+        dx = boundary[i][0] - boundary[i+1][0]
+        dy = boundary[i][1] - boundary[i+1][1]
+        norm = math.sqrt(dx**2 + dy**2)
+        dx = dx / norm * eps
+        dy = dy / norm * eps
+        b_plus[i][0] = boundary[i][0] + dy
+        b_plus[i][1] = boundary[i][1] - dx
+        b_minus[i][0] = boundary[i][0] - dy
+        b_minus[i][1] = boundary[i][1] + dx
+
+    return b_plus, b_minus
+
+def make_psi(b_plus, b_minus, vs, end_time, num):
+    psi_plus = True
+    psi_minus = True
+    t_plus = [int(b_plus[i][0]) for i in range(len(b_plus))]
+    h_plus = [b_plus[i][1] for i in range(len(b_plus))]
+    t_minus = [int(b_minus[i][0]) for i in range(len(b_minus))]
+    h_minus = [b_minus[i][1] for i in range(len(b_minus))]
+
+    for i in range(len(t_plus)):
+        temp = True
+        for j in range(int(t_plus[i] * 1.0 / end_time * num), num):
+            temp = z3.And(temp, vs[j] < h_plus[i])
+        psi_plus = z3.And(psi_plus, temp)
+
+    for i in range(len(t_minus)):
+        temp = True
+        for j in range(int(t_minus[i] * 1.0 / end_time * num), num):
+            temp = z3.And(temp, vs[j] < h_minus[i])
+
+        psi_minus = z3.And(psi_minus, z3.Not(temp))
+    psi = z3.And(psi_plus, psi_minus)
+    return psi
+
+def output_trace(phi, psi, vs, num):
+    S = z3.Solver()
+    S.add(z3.And(phi, psi))
+    print(S.check())
+    m = S.model()
+    y = [i for i in range(num)]
+    x = [i * 1.0 / 5 for i in y]
+    for i in range(num):
+        y[i] = m[vs[i]].numerator_as_long() * 1.0 / m[vs[i]].denominator_as_long()
+    trace = [[x[i], y[i]] for i in range(len(x))]
+    return trace
+    #print(y)
+    #plt.plot(x, y)
+    #plt.show()
+    
+
+def trace(boundary, eps, num, vs, end_time, phi):
+    b_plus, b_minus = pre_process(boundary, eps)
+    psi = make_psi(b_plus, b_minus, vs, end_time, num)
+    return output_trace(phi, psi, vs)
+
+
---
+++ test4.py
@@ -0,0 +1,62 @@
+import z3
+import boundary_to_trace_v4 as bt
+num = 100
+
+xs = ['x%d' % i for i in range(num)]
+us = ['u%d' % i for i in range(num)]
+
+for i in range(num):
+    xs[i] = z3.Real('x%d' % i)
+    us[i] = z3.Real('u%d' % i)
+
+
+A = 1
+B = 1
+#A = [[ Real('a_%d_%d' % (i, j)) for j in range(dim)] for i in range(dim)]
+#B = [[ Real('b_%d_%d' % (i, j)) for j in range(dim)] for i in range(dim)]
+
+def make_phi(z, w):
+    formula = True
+    for i in range(num - 1):
+    	formula = z3.And(formula, z[i+1] == A * z[i] + B * w[i], z3.And(z[i+1] - z[i] <= 0.05, z[i] - z[i+1] <= 0.05), z[i+1] != z[i], z[i] >= 0, z[i+1] >= 0)
+    	#formula = z3.And(formula, z[i+1] == A * z[i] + B * w[i], z3.Or(z[i+1] - z[i] > 0.05, z[i+1] - z[i] < -0.05), z[i] >= 0)
+        #formula = z3.And(formula, z[i+1] == A * z[i] + B * w[i], z[i+1] != z[i], z[i] >= 0, z3.Or(w[i] > 0.8, w[i] < -0.8))
+    return formula
+phi = make_phi(xs, us)
+
+tp = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+hp1 = [0.725,0.725,0.725,0.725,0.725,0.61,0.53,0.38,0.15,0.12,0.12,0.09,0.09,0.09,0.09,0.09,0.09,0.09,0.09,0.09]
+hp2 = [0.8, 0.8, 0.72, 0.65, 0.6, 0.52, 0.37, 0.25, 0.18, 0.12, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+hp3 = [0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.74, 0.72, 0.71]
+
+boundary = [[tp[i],hp1[i]] for i in range(20)]
+boundary2 = [[tp[i],hp2[i]] for i in range(20)]
+boundary3 = [[tp[i],hp3[i]] for i in range(20)]
+eps = 0.005
+end_time = 20
+
+#print(boundary)
+#print(bt.pre_process(boundary, eps, num))
+#b1, b2 = bt.pre_process(boundary, eps, num)
+#psi = bt.make_psi(b1, b2, values, end_time)
+#print(psi)
+
+#a, b = bt.pre_process(boundary, eps)
+#bt.make_psi(a, b, xs, end_time, num)
+trace = bt.trace(boundary, eps, num, xs, end_time, phi)
+
+#boundary = tb.boundary(trace)
+
+
+#print(m)
+#x = []
+#y = []
+#z = []
+#for i in range(20):
+#	x[i] = float(m[xs[i]].as_decimal(10))
+#	y[i] = float(m[ys[i]].as_decimal(10))
+#	z[i] = x[i] - y [i]
+#print(z)
+
+
+
